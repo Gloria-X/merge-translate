303a942fa1d188b83f58a204d5009f1a6fd61371
Gloria_x <656369946@qq.com>
2023-08-24 14:16:14 +0800

    update .gitignore

--- entry/mixins.js
+++
@@ -1,3 +0,0 @@
-const set = require("../feature/mixins/genExcel");
-
-set(["./../../input/cn.js", "./../../input/us.js"]);
--- entry/translate.js
+++
@@ -1,5 +0,0 @@
-const set = require("../merge/feature/translate/transJsonToExcel");
-
-set("./../../input/cn.js", "cn", ["en", "ru", "fr"], {
-  appID: "",
-});
